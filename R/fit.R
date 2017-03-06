14078c0df19a689c912d51aea1dde1ae90f36432
Kris Sankaran <sankaran.kris@gmail.com>
2017-03-06 12:05:33 -0800

    Model fitting helpers

---
+++ R/fit.R
@@ -0,0 +1,59 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## Helpers for fitting NMF / Z-NMF models using STAN.
+##
+## author: kriss1@stanford.edu
+
+## ---- modeling-helpers ----
+#' Merge Default Modeling Parameters
+#'
+#' This lets us run models with partially specified options.
+#'
+#' @param opts [list] A partially filled list of options, to fill in with
+#'   defaults.
+#' @return opts [list] The version of opts with defaults filled in.
+merge_model_opts <- function(opts = list()) {
+  default_opts <- list(
+    "inference" = "gibbs",
+    "method" = "/scratch/users/kriss1/programming/readings/nmf/src/nmf_gamma_poisson.stan",
+    "K" = 2
+  )
+  modifyList(default_opts, opts)
+}
+
+#' Fit a generic NMF Stan Model
+#'
+#' This wraps vb() and stan() in the STAN package to let us run either approach
+#' using a single command.
+#'
+#' @param y [matrix] The data on which to fit the NMF model.
+#' @param model_opts [list] A partially filled list of model fitting options.
+#'   Unspecified options will be passed into merge_model_opts().
+#' @param prior_opts [list] A list of prior information, required by the NMF
+#'   fitting STAN code.
+#' @return result [stan object] The fitted stan object.
+fit_model <- function(y, model_opts = list(), prior_opts = list()) {
+  stan_data <- list(
+    "N" = nrow(y),
+    "P" = ncol(y),
+    "y" = y,
+    "K" = model_opts$K
+  )
+  stan_data <- c(stan_data, prior_opts)
+
+  if (grepl("zero", model_opts$method)) {
+    stan_data$zero_inf_prob <- NULL
+  }
+
+  if (model_opts$inference == "gibbs") {
+    result <- stan(file = model_opts$method, data = stan_data, chain = 1)
+  } else if (model_opts$inference == "vb") {
+    f <- stan_model(model_opts$method)
+    result <- vb(f, stan_data)
+  } else {
+    stop("model_opts$inference is not recognized")
+  }
+
+  extract(result)
+}
